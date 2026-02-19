30fd9a8266f469d5fcaf172bd05f9e099d9d7790
Fabio Lopes <fabiolopes7844@gmail.com>
2026-02-18 22:37:26 -0300

    Testando Scanner

--- src/estudosjava/basico/HelloWorld.java
+++ src/estudosjava/basico/HelloWorld.java
@@ -1,7 +1,29 @@
+package estudosjava.basico;
+
+import java.util.Scanner;
+
 public class HelloWorld
 {
-    public static void main(String[] args){
-        System.out.println("Ola");
+    static void main(){
+        Scanner stringScanner = new Scanner(System.in);
+
+        System.out.println("Digite uma palavra: ");
+        String cor = stringScanner.next();
+        System.out.println(cor);
+        // Scanner.next() -> serve para ler a primeira palavra digitada.
+
+        /*
+        * É útil, após a leitura de tipos diferentes, colocar um nextLine() vazio, para limpar o buffer
+        */
+
+        stringScanner.nextLine();
+
+        System.out.println("Digite uma frase");
+        String frase = stringScanner.nextLine();
+        System.out.println(frase);
+
+
+
     }
 
 }
