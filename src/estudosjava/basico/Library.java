b50e7ed44bf93b8b3de10711a977b46fbf715b8b
Fabio Lopes <fabiolopes7844@gmail.com>
2026-02-24 11:47:24 -0300

    add Library class for managing book details and a main method for demonstration

---
+++ src/estudosjava/basico/Library.java
@@ -0,0 +1,44 @@
+package estudosjava.basico;
+
+public class Library {
+
+    private String BookName;
+    private String Actor;
+    private int Release;
+
+    public String GetBookName(){
+        return this.BookName;
+    }
+    public String GetActor(){
+        return this.Actor;
+    }
+    public int GetRelease(){
+        return this.Release;
+    }
+
+    public void SetBookName(String bookName){
+        this.BookName = bookName;
+    }
+    public void SetActor(String actor){
+        this.Actor = actor;
+    }
+    public void SetRelease(int release){
+        this.Release = release;
+    }
+}
+
+
+class CreateLibrary {
+     static void main(){
+        Library library = new Library();
+
+        library.SetBookName("Biblioteca da meia noite");
+        library.SetActor("Ator");
+        library.SetRelease(1900);
+
+        System.out.println("Nome do livro: " + library.GetBookName() + "\nAtor: " + library.GetActor() +
+                "\nAno de lançamento" + library.GetRelease());
+    }
+
+
+}
